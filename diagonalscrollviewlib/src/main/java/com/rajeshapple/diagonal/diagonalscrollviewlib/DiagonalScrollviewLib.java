d7d9d5c8346f18476e8aed77e0eae369eeaa293a
Rajesh <rajesh@speedkyat.com>
2018-07-19 18:22:44 +0630

    fff vommit

---
+++ diagonalscrollviewlib/src/main/java/com/rajeshapple/diagonal/diagonalscrollviewlib/DiagonalScrollviewLib.java
@@ -0,0 +1,15 @@
+package com.rajeshapple.diagonal.diagonalscrollviewlib;
+
+import android.util.Log;
+
+/**
+ * Created by super on 7/19/18.
+ */
+
+public class DiagonalScrollviewLib {
+    public void DataValue(String msg)
+    {
+        Log.d(DiagonalScrollviewLib.class.getClass().getSimpleName(),msg);
+
+    }
+}
